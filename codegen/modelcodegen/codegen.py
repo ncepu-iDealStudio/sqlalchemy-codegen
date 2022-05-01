adca4146036240949f029e2694c717f8e676a2e1
李瑞阳 <lryself@qq.com>
2022-05-01 16:43:44 +0800

    修复model转dict方法bug

--- codegen/modelcodegen/codegen.py
+++ codegen/modelcodegen/codegen.py
@@ -685,19 +685,27 @@ class BaseModel(db.Model):
 
     @classmethod
     def to_dict(cls, models):
-        # 查询结果为空
-        if len(models) == 0:
-            return []
-        # 查询结果不包含所有字段的情况
-        if isinstance(models[0], Row):
-            return [row._asdict() for row in models]
-        # 查询结果包含所有字段的情况
+        # 使用.all()查询的情况
+        if isinstance(models, list):
+            # 查询结果为空
+            if len(models) == 0:
+                return []
+            # 查询结果不包含所有字段的情况
+            if isinstance(models[0], Row):
+                return [row._asdict() for row in models]
+            # 查询结果包含所有字段的情况
+            else:
+                return [cls._asdict(model) for model in models]
+        # 使用.first()查询的情况
         else:
-            return [cls._asdict(model) for model in models]
+            if not models:
+                return {}
+            if isinstance(models, Row):
+                return models._asdict()
+            return cls._asdict(models)
 
     def _asdict(self):
-        return {c.key: getattr(self, c.key)
-                for c in inspect(self).mapper.column_attrs}
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
 """
             else:
                 s = """\
@@ -716,19 +724,27 @@ class Base(DeclarativeBase):
 
     @classmethod
     def to_dict(cls, models):
-        # 查询结果为空
-        if len(models) == 0:
-            return []
-        # 查询结果不包含所有字段的情况
-        if isinstance(models[0], Row):
-            return [row._asdict() for row in models]
-        # 查询结果包含所有字段的情况
+        # 使用.all()查询的情况
+        if isinstance(models, list):
+            # 查询结果为空
+            if len(models) == 0:
+                return []
+            # 查询结果不包含所有字段的情况
+            if isinstance(models[0], Row):
+                return [row._asdict() for row in models]
+            # 查询结果包含所有字段的情况
+            else:
+                return [cls._asdict(model) for model in models]
+        # 使用.first()查询的情况
         else:
-            return [cls._asdict(model) for model in models]
+            if not models:
+                return {}
+            if isinstance(models, Row):
+                return models._asdict()
+            return cls._asdict(models)
 
     def _asdict(self):
-        return {c.key: getattr(self, c.key)
-                for c in inspect(self).mapper.column_attrs}
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
 """
             outfile.write(s)
 
